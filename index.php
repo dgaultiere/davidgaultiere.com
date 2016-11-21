79e65c43d6636ecc636aa5c0e6deedf1eff63d91
David Gaultiere <dgaultiere@gmail.com>
2016-11-21 12:37:09 -0700

    modified url path for js files

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     <link href="/css/contact.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 
-		<script type="text/javascript" src="/jquery-1.3.2.min.js"></script>
-    <script type="text/javascript" src="/jquery-popup.js"></script>
+		<script type="text/javascript" src="/js/jquery-1.3.2.min.js"></script>
+    <script type="text/javascript" src="/js/jquery-popup.js"></script>
 </head>
 <!-- Contact Form -->
 	<?php include dirname(__FILE__)."/contact.php"; ?>
